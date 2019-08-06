9f9fac88b080528899058224ba0d4f3bcc2ed369
JiaEn182445u <44602073+JiaEn182445u@users.noreply.github.com>
2019-08-06 23:06:25 +0800

    Add files via upload

--- routes/cshopping.js
+++ routes/cshopping.js
@@ -1076,40 +1076,40 @@ cart.findAll(
 
 		
 		//Syahiirah Codes
-		dateSet()
+		// dateSet()
 				
-				region = doRegion(paypostalC);
-				console.log("aft payment");
-				idSort = 1;
-				sortDel.findOne({
-					where: {
-						// regionAssigned: region,
-						id: idSort
-					}
-				}).then((sort) => {
+		// 		region = doRegion(paypostalC);
+		// 		console.log("aft payment");
+		// 		idSort = 1;
+		// 		sortDel.findOne({
+		// 			where: {
+		// 				// regionAssigned: region,
+		// 				id: idSort
+		// 			}
+		// 		}).then((sort) => {
 
 
-					console.log(region);
-					console.log('aft sort');
-					console.log(sort, 'test')
-					// checksortDel(sort, region)
-					let delDate = dateDel;
-					if (sort == null) {
-						console.log('masuk null')
-						createSort(delDate, sort, paymenttt, idSort, region)
-						// sortDel.findOne({}).then((sort2)=>{
-						// 	console.log(sort2);
-						//     insertSort(sort2,region,paymenttt)
-						// })
-						// if (err) throw err
+		// 			console.log(region);
+		// 			console.log('aft sort');
+		// 			console.log(sort, 'test')
+		// 			// checksortDel(sort, region)
+		// 			let delDate = dateDel;
+		// 			if (sort == null) {
+		// 				console.log('masuk null')
+		// 				createSort(delDate, sort, paymenttt, idSort, region)
+		// 				// sortDel.findOne({}).then((sort2)=>{
+		// 				// 	console.log(sort2);
+		// 				//     insertSort(sort2,region,paymenttt)
+		// 				// })
+		// 				// if (err) throw err
 
 
-					} else {
-						wee = 1;
-						console.log('masuk tak nullllll')
-						insertSort(delDate, sort, paymenttt, idSort, wee, region)
+		// 			} else {
+		// 				wee = 1;
+		// 				console.log('masuk tak nullllll')
+		// 				insertSort(delDate, sort, paymenttt, idSort, wee, region)
 
-					}
+		// 			}
 					//end of Syahiirah's codes
 
 		cart.findAll(
@@ -1190,14 +1190,81 @@ cart.findAll(
 						(
 
 						)
+
 				}
+			//Syahiirah Codes
+		// dateSet()
+				
+		// 		region = doRegion(paypostalC);
+		// 		console.log("aft payment");
+		// 		idSort = 1;
+		// 		sortDel.findOne({
+		// 			where: {
+		// 				// regionAssigned: region,
+		// 				id: idSort
+		// 			}
+		// 		}).then((sort) => {
 
+
+		// 			console.log(region);
+		// 			console.log('aft sort');
+		// 			console.log(sort, 'test')
+		// 			// checksortDel(sort, region)
+		// 			let delDate = dateDel;
+		// 			if (sort == null) {
+		// 				console.log('masuk null')
+		// 				createSort(delDate, sort, paymenttt, idSort, region)
+		// 				// sortDel.findOne({}).then((sort2)=>{
+		// 				// 	console.log(sort2);
+		// 				//     insertSort(sort2,region,paymenttt)
+		// 				// })
+		// 				// if (err) throw err
+
+
+		// 			} else {
+		// 				wee = 1;
+		// 				console.log('masuk tak nullllll')
+		// 				insertSort(delDate, sort, paymenttt, idSort, wee, region)
+
+		// 			}
+					//end of Syahiirah's codes
 			})
-			
-	})
+			// dateSet()
+				
+			// 	region = doRegion(paypostalC);
+			// 	console.log("aft payment");
+			// 	idSort = 1;
+			// 	sortDel.findOne({
+			// 		where: {
+			// 			// regionAssigned: region,
+			// 			id: idSort
+			// 		}
+			// 	}).then((sort) => {
+
+
+			// 		console.log(region);
+			// 		console.log('aft sort');
+			// 		console.log(sort, 'test')
+			// 		// checksortDel(sort, region)
+			// 		let delDate = dateDel;
+			// 		if (sort == null) {
+			// 			console.log('masuk null')
+			// 			createSort(delDate, sort, paymenttt, idSort, region)
+			// 			// sortDel.findOne({}).then((sort2)=>{
+			// 			// 	console.log(sort2);
+			// 			//     insertSort(sort2,region,paymenttt)
+			// 			// })
+			// 			// if (err) throw err
+
+
+			// 		} else {
+			// 			wee = 1;
+			// 			console.log('masuk tak nullllll')
+			// 			insertSort(delDate, sort, paymenttt, idSort, wee, region)
+
+			// 		}
+	//})
 })
-
-
 
 		;
 
@@ -1209,6 +1276,7 @@ cart.findAll(
 			console.log("Get Payment Response");
 			console.log(JSON.stringify(paymentt));
 			// res.send('success');
+			
 			res.render('shoppingg/success')
 		}
 	});

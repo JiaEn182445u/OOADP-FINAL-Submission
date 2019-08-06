e7e2b1c6f4e6156e039669abcd86621ba5ac2829
JiaEn182445u <44602073+JiaEn182445u@users.noreply.github.com>
2019-08-06 14:17:25 +0800

    Add files via upload

---
+++ routes/m_user.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/aUser');
+const alertMessage = require('../helpers/messenger');
+var bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+
+// Login Form POST => /user/login
+router.post('/m_login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/showRegisters', // Route to /video/listVideos URL
+        failureRedirect: '/showLogin', // Route to /login URL
+        failureFlash: true
+        /* Setting the failureFlash option to true instructs Passport to flash an error
+        message using the message given by the strategy's verify callback, if any.
+        When a failure occur passport passes the message object as error */
+    })(req, res, next);
+});
+
+
+// User register URL using HTTP post => /user/register
+router.post('/m_register', (req, res) => {
+
+    let errors = [];
+    // Retrieves fields from register page from request body
+    let { name, email, password, password2 } = req.body;
+    let type = "muser";
+
+    // Checks if both passwords entered are the same
+    if (password !== password2) {
+        errors.push({ text: 'Passwords do not match' });
+    }
+
+    // Checks that password length is more than 4
+    if (password.length < 4) {
+        errors.push({ text: 'Password must be at least 4 characters' });
+    };
+    if (errors.length > 0) {
+        res.render('user/m_register', {
+            errors,
+            name,
+            email,
+            password,
+            password2,
+            type,
+        });
+    } else {
+        // If all is well, checks if user is already registered
+        User.findOne({ where: { email: req.body.email } })
+            .then(user => {
+                if (user) {
+                    // If user is found, that means email has already been
+                    // registered
+                    res.render('user/m_register', {
+                        error: user.email + ' already registered',
+                        name,
+                        email,
+                        password,
+                        password2,
+                        type,
+                    })
+                } else {
+                    // Encrypt the password
+                    var salt = bcrypt.genSaltSync(10);
+                    var hashedPassword = bcrypt.hashSync(password, salt);
+                    password = hashedPassword;
+
+                    // Create new user record
+                    User.create({ name, email, password, type })
+                        .then(user => {
+                            alertMessage(res, 'success', user.name + ' added. Please login', 'fas fa-sign-in-alt', true);
+                            res.redirect('/sshowRegister');
+                        })
+                        .catch(err => console.log(err));
+                }
+            });
+    }
+});
+
+
+router.post('/m_register', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/', (req, res) => {
+    const title = 'I\'m at the user router!';
+    res.render('index', { title: title }) // renders views/index.handlebars
+});
+
+module.exports = router; 
---
+++ routes/main_a.js
@@ -0,0 +1,171 @@
+const express = require('express');
+const router = express.Router();
+const alertMessage = require('../helpers/messenger')
+const User = require("../models/aUser")
+// const feedB = require("../models/feedback")
+const ensureAuthenticated = require("../helpers/auth")
+const fs = require('fs');
+const upload = require('../helpers/imageUpload');
+const payments = require('../models/payment');
+const orderDs = require('../models/order_detail');
+const stockRec = require('../models/stockRecord');
+// router.get('/', (req, res) => {
+// 	const title = 'Video Jotter';
+// 	res.render('index', { title: title }) // renders views/index.handlebars
+// });
+
+// liqi
+router.get('/staffLogout', (req, res) => {
+	req.logout();
+	res.redirect('/staffLogin');
+});
+
+router.get('/about', (req, res) => {
+
+	let success_msg = 'Success message';
+	let error_msg = 'Error message using error_msg';
+
+	alertMessage(res, 'success',
+		'This is an important message', 'fas fa-sign-in-alt', true);
+	alertMessage(res, 'danger',
+		'Unauthorised access', 'fas fa-exclamation-circle', false);
+
+	var errorTexts = [
+		{ text: "Error message using error object" },
+		{ text: "First error message🙅‍♀" },
+		{ text: "Second error message 🚫" },
+		{ text: "Third error message⛔" }
+	];
+
+
+	var dev_name = "🧐 Happy 脸😀"
+	res.render('about', {
+		developer_name: dev_name,
+		success_msg: success_msg,
+		error_msg: error_msg,
+		errors: errorTexts
+	}) // renders views/about.handlebars
+});
+
+router.get('/staffLogin', (req, res) => {
+	res.render('user/login')
+});
+
+
+
+router.get('/salesRecord', ensureAuthenticated, (req, res) =>{
+	User.findAll({
+	where: {
+		id: req.user.id
+	},
+	
+	raw: true
+}).then((user) => {
+	// pass object to listVideos.handlebar
+	res.render('admin/salesRecord', {
+		user: user
+	});
+}).catch(err => console.log(err));
+
+	// res.render('sale/salesRecord')
+})
+
+// router.get('/orders', (req, res) =>{
+// 	res.render('sale/orders')
+// })
+
+router.get('/orders', ensureAuthenticated, (req, res) =>{
+	User.findAll({
+	where: {
+		id: req.user.id
+	},
+	
+	raw: true
+}).then((user) => {
+	res.render('admin/orders', {
+		user: user
+	});
+}).catch(err => console.log(err));
+})
+
+router.get('/staffRegister', (req, res) => {
+	res.render('user/register')
+});
+
+
+// jiaen
+router.get('/chome', (req, res) => {
+	var d = new Date();
+
+	//var lastDay = new Date(date.getFullYear(), d.getMonth() + 1, 0);
+	//document.getElementById('trying').innerHTML = lastDay;
+	var month = d.getMonth() + 1;
+	var day = d.getDate();
+	var year = d.getFullYear();
+	// day = day + i;
+	var tmr = day + 1;
+
+
+	if (month < 10) {
+		month = '0' + month;
+	}
+	if (day < 10) {
+		day = '0' + day;
+	}
+	
+	date1 = day + "/" + month + "/" + year;
+	
+    // delMan = "Steven"
+    orderDs.findAll({
+        include: [{model : stockRec,}, {model: payments, where:{deldate: date1}}],
+
+    }).then((order) => {
+        payments.findAll({
+            // include:[{payments}],
+            // where:{
+            //     orderid: payments.id,
+            // }
+
+        }).then((payments) => {
+
+
+            // for (i = 0; i < 1000000; i++) {
+
+            // }
+            console.log(order, 'bayar');
+            res.render('chome', {
+                // order: order,
+                payments: payments,
+                order: order
+            })
+        })
+    })
+
+	// res.render('chome') 
+});
+router.get('/CRegister', (req, res) => {
+	res.render('user/cregister') // renders views/register.handlebars
+});
+
+router.get('/', (req, res) => {
+	res.render('user/clogin') // renders views/user/login.handlebars
+});
+router.get('/logout', (req, res) => {
+	req.logout();
+	res.redirect('/');
+});
+
+//Nicole
+router.get('/showLogin', (req, res) => {
+	res.render('user/m_login') // renders views/user/login.handlebars
+});
+
+router.get('/showRegister', (req, res) => {
+	res.render('user/m_register') // renders views/register.handlebars
+});
+
+router.get('/logouteu', (req, res) => {
+	req.logout();
+	res.redirect('/');
+});
+module.exports = router;
